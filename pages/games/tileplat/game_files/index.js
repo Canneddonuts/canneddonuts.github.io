a3f1d4e879c6bfea51c3b2e9331d4155e6470f76
exit0ne <Canneddonuts@protonmail.com>
2025-08-04 21:00:57 -0400

    Aug 4 25 build of TilePlat

--- pages/games/tileplat/game_files/index.js
+++ pages/games/tileplat/game_files/index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpyxmp8ycl.js
+// include: /tmp/tmpe_z3upyo.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -230,21 +230,21 @@ Module['FS_createPath']("/assets", "lvls", true, true);
 
   })();
 
-// end include: /tmp/tmpyxmp8ycl.js
-// include: /tmp/tmple4_nic2.js
+// end include: /tmp/tmpe_z3upyo.js
+// include: /tmp/tmpw1r1_dbc.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmple4_nic2.js
-// include: /tmp/tmpo9_dbokz.js
+  // end include: /tmp/tmpw1r1_dbc.js
+// include: /tmp/tmpj8cw9d0z.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpo9_dbokz.js
+  // end include: /tmp/tmpj8cw9d0z.js
 
 
 // Sometimes an existing Module object exists with properties
